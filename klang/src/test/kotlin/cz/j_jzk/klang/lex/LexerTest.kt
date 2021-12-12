e08193d58e82fca4b47853ecfb654f0036902b98
j-jzk <jjezek@protonmail.com>
2021-12-12 19:39:34 +0100

    Add tests

---
+++ klang/src/test/kotlin/cz/j_jzk/klang/lex/LexerTest.kt
@@ -0,0 +1,42 @@
+package cz.j_jzk.klang.lex
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.Ignore
+import cz.j_jzk.klang.lex.re.compileRegex
+
+/*
+ * Correct matching doesn't need to be tested extensively because it is already
+ * covered by the tests of klang-re. Things to be tested are token precendence,
+ * extracting the correct part of the input and others.
+ */
+
+class LexerTest {
+	private val lexer = Lexer<String>(mapOf(
+		re("if") to "if",
+		re("\\d+") to "int"
+	))
+
+	@Test fun testBasicLex() {
+		val input = iter("if123")
+
+		assertEquals(Token("if", "if"), lexer.nextToken(input))
+		assertEquals(Token("int", "123"), lexer.nextToken(input))
+	}
+
+	@Ignore @Test fun testNoMatch() {
+		// test if the lexer throws an error of some sort on no match
+		// (maybe don't check this in the lexer, but instead use some sort of error token)
+		TODO()
+	}
+
+	@Test fun testEndOfInput() {
+		val input = iter("if")
+		lexer.nextToken(input)
+
+		assertEquals(null, lexer.nextToken(input))
+	}
+
+	private fun re(str: String) = compileRegex(str).fa
+	private fun iter(str: String) = str.toList().listIterator()
+}
