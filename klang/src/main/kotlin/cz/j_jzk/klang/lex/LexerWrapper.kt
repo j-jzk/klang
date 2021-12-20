b7b3c6ecd99ffecdc6e538dadfddc82bb49b9534
j-jzk <jjezek@protonmail.com>
2021-12-20 18:48:22 +0100

    Make the iterator return non-null values

--- klang/src/main/kotlin/cz/j_jzk/klang/lex/LexerWrapper.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/lex/LexerWrapper.kt
@@ -19,8 +19,29 @@ class LexerWrapper<T>(val lexer: Lexer<T>, private val onNoMatch: (Char) -> Unit
 		return match
 	}
 
-	fun iterator(input: ListIterator<Char>) = object : Iterator<Token<T>?> {
-		override fun hasNext() = input.hasNext()
-		override fun next() = nextMatch(input)
+	fun iterator(input: ListIterator<Char>) = LexerIterator(input)
+
+	inner class LexerIterator(private val input: ListIterator<Char>): Iterator<Token<T>> {
+		/* We actually have to load tokens ahead of time to know if there are any
+		 * or if it's just invalid characters and EOF. */
+		private var nextValue: Token<T>? = null
+		init {
+			loadNextValue()
+		}
+
+		private fun loadNextValue() {
+			nextValue =
+				if (input.hasNext())
+					nextMatch(input)
+				else
+					null
+		}
+
+		override fun hasNext() = nextValue != null
+		override fun next() =
+			nextValue?.let {
+				loadNextValue()
+				it
+			} ?: throw NoSuchElementException()
 	}
 }
