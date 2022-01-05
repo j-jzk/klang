934a071f89091686d7e1d5027f5e2867b24f7a6e
j-jzk <jjezek@protonmail.com>
2022-01-05 15:33:55 +0100

    Add a factory for IdentifiableInputs

---
+++ klang/src/main/kotlin/cz/j_jzk/klang/input/IdentifiableInputFactory.kt
@@ -0,0 +1,16 @@
+package cz.j_jzk.klang.input
+
+import java.io.FileInputStream
+
+/**
+ * An object for creating IdentifiableInputs from common sources, like files.
+ * 
+ * However, this is not the only way to supply data to your lexer. You can use
+ * any class which implements ListIterator, or use InputListIterator to convert
+ * an InputStream to a ListIterator.
+ */
+object InputFactory {
+    public fun fromFile(path: String) = IdentifiableInput(path, InputListIterator(FileInputStream(path)))
+	public fun fromStdin() = IdentifiableInput("STDIN", InputListIterator(System.in))
+	public fun fromString(string: String, id: String) = IdentifiableInput(id, string.toList().listIterator())
+}
