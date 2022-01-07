7747659fe7a40ddb5c3f0790a030907bf10e8ab3
j-jzk <jjezek@protonmail.com>
2022-01-07 18:25:48 +0100

    Add tests

--- klang/src/test/kotlin/cz/j_jzk/klang/lex/LexerIntegrationTest.kt
+++ klang/src/test/kotlin/cz/j_jzk/klang/lex/LexerIntegrationTest.kt
@@ -3,7 +3,11 @@ package cz.j_jzk.klang.lex
 import cz.j_jzk.klang.lex.api.lexer
 import cz.j_jzk.klang.testutils.testLex
 import cz.j_jzk.klang.testutils.FToken
+import cz.j_jzk.klang.input.InputFactory
+import cz.j_jzk.klang.util.PositionInfo
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 class LexerIntegrationTest {
 	@Test fun testBasicLexer() {
@@ -26,5 +30,30 @@ class LexerIntegrationTest {
 				FToken("IF", "if")
 			)
 		)
+	}
+
+	@Test fun testOnNoMatch() {
+		var onNoMatchInvocations = 0
+		val lexer = lexer<String> {
+			"INT" to """\d+"""
+			ignore(" ")
+			onNoMatch {
+				onNoMatchInvocations++
+			}
+		}.getLexer()
+		val input = InputFactory.fromString("10  123x5 :)", "input")
+
+		val expectedTokens = listOf(
+			Token("INT", "10", PositionInfo("input", 0)),
+			Token("INT", "123", PositionInfo("input", 4)),
+			Token("INT", "5", PositionInfo("input", 8)),
+		).iterator()
+
+		for (token in lexer.iterator(input)) {
+			assertEquals(expectedTokens.next(), token)
+		}
+
+		assertFalse(expectedTokens.hasNext())
+		assertEquals(3, onNoMatchInvocations)
 	}
 }
