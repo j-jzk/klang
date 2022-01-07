6939242d5157a8bf5ea88639bf50c22944b2e4b9
j-jzk <jjezek@protonmail.com>
2022-01-07 16:13:12 +0100

    Increase code and API quality (#18)
    * Add Javadoc for public interfaces
    * Increase code readability

--- klang/src/main/kotlin/cz/j_jzk/klang/lex/LexerWrapper.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/lex/LexerWrapper.kt
@@ -2,30 +2,35 @@ package cz.j_jzk.klang.lex
 
 import cz.j_jzk.klang.input.IdentifiableInput
 
+/**
+ * A utility class for using the lexer more easily and safely.
+ * It also calls functions on certain events (currently only no match).
+ */
 class LexerWrapper<T>(val lexer: Lexer<T>, private val onNoMatch: (Char) -> Unit) {
 	/**
 	 * Matches one token and handles events (onNoMatch).
 	 * When there is no match, it automatically returns the next one, but can
 	 * still return null when there are no matches up to the end of input.
 	 */
-	private fun nextMatch(input: IdentifiableInput): Token<T>? {
+	private tailrec fun nextMatch(input: IdentifiableInput): Token<T>? {
+		if (!input.input.hasNext()) return null
+
 		val match = lexer.nextToken(input)
-		// TODO untangle this mess
-		if (match == null) {
-			if (input.input.hasNext()) {
-				onNoMatch(input.input.next())
-				return if (input.input.hasNext())
-					nextMatch(input)
-				else
-					null
-			} else {
-				return null
-			}
+
+		if (match == null && input.input.hasNext()) {
+			onNoMatch(input.input.next())
+			return nextMatch(input)
 		}
 
 		return match
 	}
 
+	/**
+	 * Returns an iterator over the tokens of an input.
+	 * For example:
+	 *     for (token in lexer.iterator(input)
+	 *         println(token)
+	 */
 	fun iterator(input: IdentifiableInput) = LexerIterator(input)
 
 	inner class LexerIterator(private val input: IdentifiableInput): Iterator<Token<T>> {
