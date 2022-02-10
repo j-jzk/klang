151a339a9681bc17be8c8f837022e0d74d0e4e58
j-jzk <jjezek@protonmail.com>
2022-02-10 16:07:47 +0100

    Add Javadoc for ASTNode

--- klang/src/main/kotlin/cz/j_jzk/klang/parse/ASTNode.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/ASTNode.kt
@@ -1,12 +1,12 @@
 package cz.j_jzk.klang.parse
 
-/**
- * Represents a node of the AST.
- */
+/** This interface represents a node of the AST. */
 // Do we need the type parameter N?
 sealed interface ASTNode<out N> {
 	val id: NodeID
 
+	/** A regular node of the AST. */
 	data class Data<out N>(override val id: NodeID, val data: N): ASTNode<N>
+	/** A dummy AST node used for error recovery. */
 	data class Errorneous<N>(override val id: NodeID): ASTNode<N>
 }
