ec0e14e409cc3c1e160c7b4232090aae3296cd73
j-jzk <jjezek@protonmail.com>
2022-02-11 13:42:56 +0100

    Add a basic API for creating a parser (#33)

---
+++ klang/src/test/kotlin/cz/j_jzk/klang/parse/ParserIntegrationTest.kt
@@ -0,0 +1,50 @@
+package cz.j_jzk.klang.parse
+
+import cz.j_jzk.klang.parse.api.parser
+import java.lang.IllegalArgumentException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class ParserIntegrationTest {
+	private val additionParser = parser<String, Int> {
+		"top" to def("expr2") { it[0] }
+		"expr2" to def("expr2", "plus", "expr") { it[0] + it[2] }
+		"expr2" to def("expr") { it[0] }
+
+		topNode = "top"
+
+		errorRecovering("top", "expr2")
+	}.getParser()
+
+	@Test fun testBasicParser() {
+		val input = createInput("5 + 10 + 1")
+
+		val result = additionParser.parse(input)
+		assertTrue(result is ASTNode.Data)
+		assertEquals(16, result.data)
+	}
+
+	@Test fun testParserWithoutTopNode() {
+		assertFailsWith(IllegalArgumentException::class) {
+			parser<String, Int> {
+				"foo" to def("bar") { it[0] }
+			}.getParser()
+		}
+	}
+
+	@Test fun testParserWithErroneousInput() {
+		val input = createInput("5 + 1 2 + 3")
+		val result = additionParser.parse(input)
+		assertTrue(result is ASTNode.Erroneous)
+	}
+
+	private fun createInput(input: String) =
+		(input.split(" ").map { tok ->
+			if (tok == "+")
+				ASTNode.Data(NodeID.ID("plus"), 0)
+			else
+				ASTNode.Data(NodeID.ID("expr"), tok.toInt())
+		} + ASTNode.Data(NodeID.Eof, 0)).iterator() // TODO: how should we handle EOF nodes IRL?
+}
