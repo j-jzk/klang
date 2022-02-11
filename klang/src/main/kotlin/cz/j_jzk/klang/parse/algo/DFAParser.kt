61fa3c6d997908dcb4fe9f4636851615e8711d99
j-jzk <jjezek@protonmail.com>
2022-02-11 13:42:56 +0100

    Add a basic API for creating a parser (#33)

--- klang/src/main/kotlin/cz/j_jzk/klang/parse/algo/DFAParser.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/algo/DFAParser.kt
@@ -14,7 +14,10 @@ data class DFA<N>(
 	val finalNodeType: NodeID,
 	val startState: State,
 	val errorRecoveringNodes: List<NodeID>,
-)
+) {
+	/** Runs the parser and returns the resulting syntax tree */
+	fun parse(input: Iterator<ASTNode<N>>) = DFAParser(input, this).parse()
+}
 
 /**
  * This class contains all the logic of the parser.
@@ -22,7 +25,7 @@ data class DFA<N>(
  * Because the parser needs some mutable state, this is kept separate from
  * the DFA in order to allow using the same DFA on different inputs.
  */
-class DFAParser<N>(input: Iterator<ASTNode<N>>, val dfa: DFA<N>) {
+internal class DFAParser<N>(input: Iterator<ASTNode<N>>, val dfa: DFA<N>) {
 	private val nodeStack = mutableListOf<ASTNode<N>>() // TODO better data type?
 	private val stateStack = mutableListOf(dfa.startState)
 
---
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/api/ParserBuilder.kt
@@ -0,0 +1,83 @@
+package cz.j_jzk.klang.parse.api
+
+import org.apache.commons.collections4.map.LazyMap
+import cz.j_jzk.klang.parse.NodeID
+import cz.j_jzk.klang.parse.NodeDef
+import cz.j_jzk.klang.parse.ASTNode
+import cz.j_jzk.klang.parse.algo.DFA
+import cz.j_jzk.klang.parse.algo.DFABuilder
+
+/**
+ * A function to create a parser.
+ *
+ * Type parameters:
+ *   I - A type for identifying nodes of the AST. Enums are the most suitable
+ *       for this, but you may as well use anything you want
+ *   D - The type of the data stored in the AST
+ */
+fun <I, D> parser(init: ParserBuilder<I, D>.() -> Unit) = ParserBuilder<I, D>().also { it.init() }
+
+/**
+ * An interface for building a parser. You probably don't want to use this
+ * directly, but instead use the function parser() from this package.
+ */
+class ParserBuilder<I, D> {
+	private val actualNodeDefs = mutableMapOf<NodeID, MutableSet<NodeDef<D>>>()
+	private val nodeDefs = LazyMap.lazyMap<NodeID, MutableSet<NodeDef<D>>>(actualNodeDefs) { -> mutableSetOf() }
+	private val errorRecoveringNodes = mutableListOf<NodeID>()
+
+	/** Maps a node to its definition. */
+	infix fun I.to(definition: IntermediateNodeDefinition<I, D>) {
+		val actualID = NodeID.ID(this)
+		val actualDefinition = definition.definition.map { NodeID.ID(it) }
+		val actualReduction = wrapReduction(actualID, definition.reduction)
+		nodeDefs[actualID]!!.add(NodeDef(actualDefinition, actualReduction))
+	}
+
+	/** Creates a node definition */
+	fun def(vararg definition: I, reduction: (List<D>) -> D) = IntermediateNodeDefinition(definition.toList(), reduction)
+
+	/** The top node of the grammar (the root of the AST) */
+	var topNode: I? = null
+
+	/**
+	 * Marks `nodes` to be error-recovering. These nodes will be then used to
+	 * contain syntax errors.
+	 */
+	fun errorRecovering(vararg nodes: I) {
+		errorRecoveringNodes += nodes.map { NodeID.ID(it) }
+	}
+
+	/** Builds and returns the parser */
+	fun getParser(): DFA<D> {
+		requireNotNull(topNode) { "The top node of the grammar must be set" }
+		return DFABuilder(actualNodeDefs, NodeID.ID(topNode), errorRecoveringNodes).build()
+	}
+
+	/**
+	 * Returns an altered reduction function which:
+	 *   - translates the parameters and return values to/from `ASTNode`
+	 *   - if any of the nodes to be reduced is `Erroneous`, it also returns
+	 *     an Erroneous node, because the reduction couldn't work with it
+	 *     (it has no value) and a correct program couldn't be created from
+	 *     such an AST anyway
+	 */
+	private fun wrapReduction(nodeID: NodeID, reduction: (List<D>) -> D): (List<ASTNode<D>>) -> ASTNode<D> =
+		{ nodeList ->
+			if (nodeList.none { it is ASTNode.Erroneous })
+				ASTNode.Data(
+						nodeID,
+						reduction(nodeList.map { node ->
+							(node as ASTNode.Data<D>).data
+						})
+				)
+			else
+				ASTNode.Erroneous(nodeID)
+		}
+
+	/**
+	 * A structure used internally to represent a node definition. (This is
+	 * used to allow for a syntax with fewer braces.)
+	 */
+	data class IntermediateNodeDefinition<I, D>(val definition: List<I>, val reduction: (List<D>) -> D)
+}
