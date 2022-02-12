8934431f88c59856e51c8344e26fd932afa6bf99
j-jzk <jjezek@protonmail.com>
2022-02-12 19:08:29 +0100

    Handle EOF properly

--- klang/src/main/kotlin/cz/j_jzk/klang/parse/TokenConverter.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/TokenConverter.kt
@@ -3,8 +3,15 @@ package cz.j_jzk.klang.parse
 import cz.j_jzk.klang.lex.Token
 
 class TokenConverter<I, D>(private val conversions: Map<I, (String) -> D>, private val input: Iterator<Token<I>>): Iterator<ASTNode<D>> {
-	override fun hasNext() = input.hasNext()
+	/* This is for tracking if we returned an EOF already - we want to return
+	 * it only once */
+	private var gotEOF = false
+
+	override fun hasNext() = input.hasNext() || !gotEOF
 	override fun next(): ASTNode<D> {
+		if (!input.hasNext() && !gotEOF)
+			return ASTNode.NoValue(NodeID.Eof)
+
 		val token = input.next()
 		val id = NodeID.ID(token.id)
 		val conversion = conversions[token.id] ?: return ASTNode.NoValue(id)
