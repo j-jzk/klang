9c5686ddb6bc6239dabe52892e7ddc4eee26de7d
j-jzk <jjezek@protonmail.com>
2022-04-04 14:36:12 +0200

    Decouple TokenConverter from LexerIterator

--- klang/src/main/kotlin/cz/j_jzk/klang/parse/TokenConverter.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/TokenConverter.kt
@@ -1,13 +1,19 @@
 package cz.j_jzk.klang.parse
 
 import cz.j_jzk.klang.lex.LexerWrapper
+import cz.j_jzk.klang.lex.Token
 import cz.j_jzk.klang.util.PositionInfo
+import cz.j_jzk.klang.input.IdentifiableInput
 
 /** An iterator that transforms Tokens from an input using specified conversion functions */
 class TokenConverter<I, D>(
 	private val conversions: Map<I, (String) -> D>,
-	private val input: LexerWrapper<I>.LexerIterator
+	private val input: Iterator<Token<I>>,
+	private val identifiableInput: IdentifiableInput
 ): Iterator<ASTNode<D>> {
+	constructor(conversions: Map<I, (String) -> D>, input: LexerWrapper<I>.LexerIterator):
+		this(conversions, input, input.input)
+
 	/* This is for tracking if we returned an EOF already - we want to return
 	 * it only once */
 	private var gotEOF = false
@@ -15,7 +21,7 @@ class TokenConverter<I, D>(
 	override fun hasNext() = input.hasNext() || !gotEOF
 	override fun next(): ASTNode<D> {
 		if (!input.hasNext() && !gotEOF)
-			return ASTNode.NoValue(NodeID.Eof, PositionInfo(input.input.id, input.input.input.previousIndex()))
+			return ASTNode.NoValue(NodeID.Eof, PositionInfo(identifiableInput.id, identifiableInput.input.previousIndex()))
 		if (!hasNext())
 			throw NoSuchElementException()
 
