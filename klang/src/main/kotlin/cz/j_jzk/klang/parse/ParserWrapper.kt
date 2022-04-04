c4d360f333a3a753add818d49c9b3d4ef3b295c6
j-jzk <jjezek@protonmail.com>
2022-04-04 13:08:13 +0200

    Decouple ParserWrapper from the LexerIterator

--- klang/src/main/kotlin/cz/j_jzk/klang/parse/ParserWrapper.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/ParserWrapper.kt
@@ -4,14 +4,21 @@ import cz.j_jzk.klang.parse.algo.DFA
 import cz.j_jzk.klang.lex.LexerWrapper
 
 /** A utility class to simplify the interop between the lexer and the parser */
-class ParserWrapper<I, D>(val dfa: DFA<D>, private val tokenConversions: Map<I, (String) -> D>) {
+class ParserWrapper<I, D>(val dfa: DFA<D>, val tokenConversions: Map<I, (String) -> D>) {
 	/**
 	 * Parses the tokens from the `input`.
 	 * @param input A stream of tokens, ideally from LexerWrapper.iterator()
 	 * @throws SyntaxError if there were any syntax errors in the input
 	 */
-	fun parse(input: LexerWrapper<I>.LexerIterator): D {
-		val result = dfa.parse(TokenConverter(tokenConversions, input))
+	fun parse(input: LexerWrapper<I>.LexerIterator) = parse(TokenConverter(tokenConversions, input))
+
+	/**
+	 * Parses the AST nodes from `input`.
+	 * @param input A stream of unprocessed AST nodes
+	 * @throws SyntaxError if there were any syntax errors in the input
+	 */
+	fun parse(input: Iterator<ASTNode<D>>): D {
+		val result = dfa.parse(input)
 		if (result is ASTNode.Erroneous)
 			throw SyntaxError()
 		else
