2f16829ae6640d6b9c687d0e998dcda51e3c254a
j-jzk <jjezek@protonmail.com>
2023-01-27 13:44:33 +0100

    Add logic operators

--- klang-prales/src/main/kotlin/cz/j_jzk/klang/prales/operators.kt
+++ klang-prales/src/main/kotlin/cz/j_jzk/klang/prales/operators.kt
@@ -3,44 +3,77 @@ package cz.j_jzk.klang.prales.operators;
 import cz.j_jzk.klang.lesana.lesana
 import cz.j_jzk.klang.parse.NodeID
 
-sealed class Operation<E> {
+sealed class Oper<E> {
     /** Identity (a) */
-    data class Id<E>(val a: E): Operation<E>()
-    /** Addition (a + b) */
-    data class Add<E>(val a: Operation<E>, val b: Operation<E>): Operation<E>()
-    /** Subtraction (a - b) */
-    data class Sub<E>(val a: Operation<E>, val b: Operation<E>): Operation<E>()
-    /** Multiplication (a * b) */
-    data class Mul<E>(val a: Operation<E>, val b: Operation<E>): Operation<E>()
-    /** Division (a / b) */
-    data class Div<E>(val a: Operation<E>, val b: Operation<E>): Operation<E>()
-    /** Modulo (a % b) */
-    data class Mod<E>(val a: Operation<E>, val b: Operation<E>): Operation<E>()
-    /** Negation (-a) */
-    data class Neg<E>(val a: Operation<E>): Operation<E>()
+    data class Id<E>(val a: E): Oper<E>()
+
+    /** A namespace for arithmetic operations */
+    object Arit {
+        /** Addition (a + b) */
+        data class Add<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+
+        /** Subtraction (a - b) */
+        data class Sub<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+
+        /** Multiplication (a * b) */
+        data class Mul<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+
+        /** Division (a / b) */
+        data class Div<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+
+        /** Modulo (a % b) */
+        data class Mod<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+
+        /** Negation (-a) */
+        data class Neg<E>(val a: Oper<E>): Oper<E>()
+    }
+
+    /** A namespace for logic operations */
+    object Log {
+        /** Negation (!a) */
+        data class Not<E>(val a: Oper<E>): Oper<E>()
+        /** Conjunction (a && b) */
+        data class And<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+        /** Disjunction (a || b) */
+        data class Or<E>(val a: Oper<E>, val b: Oper<E>): Oper<E>()
+    }
 }
 
-fun <E> operators(expr: NodeID<E>) = lesana<Operation<E>> {
+fun <E> operators(expr: NodeID<E>) = lesana<Oper<E>> {
     // from highest priority to lowest priority
-    val lvl1 = NodeID<Operation<E>>()
-    lvl1 to def(expr) { Operation.Id(it.v1) }
+    // ARITHMETIC OPERATIONS
+    val lvl1 = NodeID<Oper<E>>()
+    lvl1 to def(expr) { Oper.Id(it.v1) }
 
-    val lvl2 = NodeID<Operation<E>>()
-    lvl2 to def(lvl2, re("\\*"), lvl1) { (a, _, b) -> Operation.Mul(a, b) }
-    lvl2 to def(lvl2, re("/"), lvl1) { (a, _, b) -> Operation.Div(a, b) }
-    lvl2 to def(lvl2, re("%"), lvl1) { (a, _, b) -> Operation.Mod(a, b) }
+    val lvl2 = NodeID<Oper<E>>()
+    lvl2 to def(lvl2, re("\\*"), lvl1) { (a, _, b) -> Oper.Arit.Mul(a, b) }
+    lvl2 to def(lvl2, re("/"), lvl1) { (a, _, b) -> Oper.Arit.Div(a, b) }
+    lvl2 to def(lvl2, re("%"), lvl1) { (a, _, b) -> Oper.Arit.Mod(a, b) }
     lvl2 to def(lvl1) { it.v1 }
 
-    val lvl3 = NodeID<Operation<E>>()
-    lvl3 to def(lvl3, re("\\+"), lvl2) { (a, _, b) -> Operation.Add(a, b) }
-    lvl3 to def(lvl3, re("-"), lvl2) { (a, _, b) -> Operation.Sub(a, b) }
-    lvl3 to def(re("-"), lvl2) { (_, a) -> Operation.Neg(a) }
+    val lvl3 = NodeID<Oper<E>>()
+    lvl3 to def(lvl3, re("\\+"), lvl2) { (a, _, b) -> Oper.Arit.Add(a, b) }
+    lvl3 to def(lvl3, re("-"), lvl2) { (a, _, b) -> Oper.Arit.Sub(a, b) }
+    lvl3 to def(re("-"), lvl2) { (_, a) -> Oper.Arit.Neg(a) }
     lvl3 to def(lvl2) { it.v1 }
 
-    lvl1 to def(re("\\("), lvl3, re("\\)")) { it.v2 }
+    // LOGIC OPERATIONS
+    val lvl4 = NodeID<Oper<E>>()
+    lvl4 to def(re("!"), lvl3) { (_, a) -> Oper.Log.Not(a) }
+    lvl4 to def(lvl3) { it.v1 }
 
-    val top = NodeID<Operation<E>>()
-    top to def(lvl3) { it.v1 }
+    val lvl5 = NodeID<Oper<E>>()
+    lvl5 to def(lvl5, re("&&"), lvl4) { (a, _, b) -> Oper.Log.And(a, b) }
+    lvl5 to def(lvl4) { it.v1 }
+
+    val lvl6 = NodeID<Oper<E>>()
+    lvl6 to def(lvl6, re("\\|\\|"), lvl5) { (a, _, b) -> Oper.Log.Or(a, b) }
+    lvl6 to def(lvl5) { it.v1 }
+
+    lvl1 to def(re("\\("), lvl6, re("\\)")) { it.v2 }
+
+    val top = NodeID<Oper<E>>()
+    top to def(lvl6) { it.v1 }
     setTopNode(top)
     inheritIgnoredREs()
 }
--- klang-prales/src/test/kotlin/cz/j_jzk/klang/prales/operators/ArithmeticTest.kt
+++ klang-prales/src/test/kotlin/cz/j_jzk/klang/prales/operators/ArithmeticTest.kt
@@ -5,12 +5,12 @@ import cz.j_jzk.klang.prales.testLesana
 import cz.j_jzk.klang.lesana.lesana
 import cz.j_jzk.klang.parse.NodeID
 
-class ArithmeticTest {
-    @Test fun testBasic() {
-        val lesana = lesana<ArithmeticOperation<Int>> {
+class OperatorTest {
+    @Test fun testArithmetic() {
+        val lesana = lesana<Oper<Int>> {
             val int = NodeID<Int>()
             int to def(re("[0-9]+")) { it.v1.toInt() }
-            val operators = include(arithmetic(int))
+            val operators = include(operators(int))
             setTopNode(operators)
             ignoreRegexes("\\s+")
         }
@@ -29,6 +29,7 @@ class ArithmeticTest {
 
                 "1 * 2 + 3" to add(mul(i(1), i(2)), i(3)),
                 "1 + 2 * 3" to add(i(1), mul(i(2), i(3))),
+                "1 * 2 / 3 * 4 / 5" to div(mul(div(mul(i(1), i(2)), i(3)), i(4)), i(5)),
                 "(1 + 2) * 3" to mul(add(i(1), i(2)), i(3)),
                 "- 1 + 2" to add(neg(i(1)), i(2)),
                 "1 + (-2)" to add(i(1), neg(i(2))),
@@ -43,15 +44,62 @@ class ArithmeticTest {
                 "()",
                 "(1",
                 "1)",
+                "1 && 2",
+                "!1",
+                "1 || 2",
             ),
         )
     }
 
-    private fun i(value: Int) = ArithmeticOperation.Id(value)
-    private fun add(a: ArithmeticOperation<Int>, b: ArithmeticOperation<Int>) = ArithmeticOperation.Add(a, b)
-    private fun sub(a: ArithmeticOperation<Int>, b: ArithmeticOperation<Int>) = ArithmeticOperation.Sub(a, b)
-    private fun mul(a: ArithmeticOperation<Int>, b: ArithmeticOperation<Int>) = ArithmeticOperation.Mul(a, b)
-    private fun div(a: ArithmeticOperation<Int>, b: ArithmeticOperation<Int>) = ArithmeticOperation.Div(a, b)
-    private fun mod(a: ArithmeticOperation<Int>, b: ArithmeticOperation<Int>) = ArithmeticOperation.Mod(a, b)
-    private fun neg(a: ArithmeticOperation<Int>) = ArithmeticOperation.Neg(a)
+    @Test fun testLogic() {
+        val lesana = lesana<Oper<Int>> {
+            val int = NodeID<Int>()
+            int to def(re("[0-9]+")) { it.v1.toInt() }
+            val operators = include(operators(int))
+            setTopNode(operators)
+            ignoreRegexes("\\s+")
+        }
+
+        testLesana(
+            lesana.getLesana(),
+            mapOf(
+                "1" to i(1),
+                "1 && 2" to and(i(1), i(2)),
+                "1 || 2" to or(i(1), i(2)),
+                "!1" to not(i(1)),
+
+                "1 && 2 || 3" to or(and(i(1), i(2)), i(3)),
+                "1 || 2 && 3" to or(i(1), and(i(2), i(3))),
+                "(1 || 2) && 3" to and(or(i(1), i(2)), i(3)),
+                "1 && 2 || !3" to or(and(i(1), i(2)), not(i(3)))
+            ),
+            listOf(
+                "1 + -2",
+                "2 * -1",
+                "1 ++ 2",
+                "1+",
+                "+1",
+                "()",
+                "(1",
+                "1)",
+                "1 + 2",
+                "1 * 2",
+                "-1",
+                "1 / 2",
+                "1 * 2",
+                "i % 2",
+            ),
+        )
+    }
+
+    private fun i(value: Int) = Oper.Id(value)
+    private fun add(a: Oper<Int>, b: Oper<Int>) = Oper.Arit.Add(a, b)
+    private fun sub(a: Oper<Int>, b: Oper<Int>) = Oper.Arit.Sub(a, b)
+    private fun mul(a: Oper<Int>, b: Oper<Int>) = Oper.Arit.Mul(a, b)
+    private fun div(a: Oper<Int>, b: Oper<Int>) = Oper.Arit.Div(a, b)
+    private fun mod(a: Oper<Int>, b: Oper<Int>) = Oper.Arit.Mod(a, b)
+    private fun neg(a: Oper<Int>) = Oper.Arit.Neg(a)
+    private fun not(a: Oper<Int>) = Oper.Log.Not(a)
+    private fun and(a: Oper<Int>, b: Oper<Int>) = Oper.Log.And(a, b)
+    private fun or(a: Oper<Int>, b: Oper<Int>) = Oper.Log.Or(a, b)
 }
