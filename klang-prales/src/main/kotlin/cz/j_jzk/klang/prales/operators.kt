1e74c463b494e528a71053b1923fd4902c5e0870
j-jzk <jjezek@protonmail.com>
2023-02-05 15:10:18 +0100

    Take advantage of the NodeID descriptions

--- klang-prales/src/main/kotlin/cz/j_jzk/klang/prales/operators.kt
+++ klang-prales/src/main/kotlin/cz/j_jzk/klang/prales/operators.kt
@@ -42,17 +42,17 @@ sealed class Oper<E> {
 fun <E> operators(expr: NodeID<E>, arithmetic: Boolean = true, logic: Boolean = true) = lesana<Oper<E>> {
     // from highest to lowest priority
     // ARITHMETIC OPERATIONS
-    val lvl1 = NodeID<Oper<E>>()
+    val lvl1 = NodeID<Oper<E>>(show=false)
     lvl1 to def(expr) { Oper.Id(it.v1) }
 
     val aritTop = if (arithmetic) {
-        val lvl2 = NodeID<Oper<E>>()
+        val lvl2 = NodeID<Oper<E>>(show=false)
         lvl2 to def(lvl2, re("\\*"), lvl1) { (a, _, b) -> Oper.Arit.Mul(a, b) }
         lvl2 to def(lvl2, re("/"), lvl1) { (a, _, b) -> Oper.Arit.Div(a, b) }
         lvl2 to def(lvl2, re("%"), lvl1) { (a, _, b) -> Oper.Arit.Mod(a, b) }
         lvl2 to def(lvl1) { it.v1 }
 
-        val lvl3 = NodeID<Oper<E>>()
+        val lvl3 = NodeID<Oper<E>>("arithmetic operation")
         lvl3 to def(lvl3, re("\\+"), lvl2) { (a, _, b) -> Oper.Arit.Add(a, b) }
         lvl3 to def(lvl3, re("-"), lvl2) { (a, _, b) -> Oper.Arit.Sub(a, b) }
         lvl3 to def(re("-"), lvl2) { (_, a) -> Oper.Arit.Neg(a) }
@@ -65,15 +65,15 @@ fun <E> operators(expr: NodeID<E>, arithmetic: Boolean = true, logic: Boolean = 
 
     // LOGIC OPERATIONS
     val logTop = if (logic) {
-        val lvl4 = NodeID<Oper<E>>()
+        val lvl4 = NodeID<Oper<E>>(show=false)
         lvl4 to def(re("!"), aritTop) { (_, a) -> Oper.Log.Not(a) }
         lvl4 to def(aritTop) { it.v1 }
 
-        val lvl5 = NodeID<Oper<E>>()
+        val lvl5 = NodeID<Oper<E>>(show=false)
         lvl5 to def(lvl5, re("&&"), lvl4) { (a, _, b) -> Oper.Log.And(a, b) }
         lvl5 to def(lvl4) { it.v1 }
 
-        val lvl6 = NodeID<Oper<E>>()
+        val lvl6 = NodeID<Oper<E>>("logic operation")
         lvl6 to def(lvl6, re("\\|\\|"), lvl5) { (a, _, b) -> Oper.Log.Or(a, b) }
         lvl6 to def(lvl5) { it.v1 }
 
@@ -84,7 +84,7 @@ fun <E> operators(expr: NodeID<E>, arithmetic: Boolean = true, logic: Boolean = 
 
     lvl1 to def(re("\\("), logTop, re("\\)")) { it.v2 }
 
-    val top = NodeID<Oper<E>>()
+    val top = NodeID<Oper<E>>(show=false)
     top to def(logTop) { it.v1 }
     setTopNode(top)
     inheritIgnoredREs()
