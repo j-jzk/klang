b5c1f8d4e5356eabb12665b64033031887942505
j-jzk <jjezek@protonmail.com>
2023-03-24 15:39:52 +0100

    Update KDoc in klang-lesana

--- klang/src/main/kotlin/cz/j_jzk/klang/input/InputFactory.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/input/InputFactory.kt
@@ -3,10 +3,10 @@ package cz.j_jzk.klang.input
 import java.io.FileInputStream
 
 /**
- * An object for creating IdentifiableInputs from common sources, like files.
+ * An object for creating [IdentifiableInput]s from common sources, like files.
  *
- * However, this is not the only way to supply data to your lexer. You can use
- * any class which implements ListIterator, or use InputListIterator to convert
+ * However, this is not the only way to supply data to your lesana. You can use
+ * any class which implements [ListIterator], or use [InputListIterator] to convert
  * an InputStream to a ListIterator.
  */
 @Suppress("UndocumentedPublicFunction")
--- klang/src/main/kotlin/cz/j_jzk/klang/parse/UnexpectedTokenError.kt
+++ klang/src/main/kotlin/cz/j_jzk/klang/parse/UnexpectedTokenError.kt
@@ -3,6 +3,10 @@ package cz.j_jzk.klang.parse
 /**
  * This class includes information about an unexpected token error -
  * what tokens were expected and what did we get
+ * 
+ * @property expectedIDs What did we expect
+ * @property got What did we get - the unexpected token (also contains information
+ *  about its position = the position of the error)
  */
 data class UnexpectedTokenError(
     val got: ASTNode,
